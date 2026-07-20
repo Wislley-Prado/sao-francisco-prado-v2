33f1912324ede2027f3b1335fc28b5944a628e4d
Wislley Prado <franco@MacBook-Air-M3-Wislley-2.local>
2026-07-20 06:38:46 -0300

    refactor: limpa importacoes e otimiza renderizacao do grafico de evolucao da represa

--- src/components/dam/DamHistoryCharts.tsx
+++ src/components/dam/DamHistoryCharts.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { 
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,
-  Legend, Area, ComposedChart, Bar
+  Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,
+  Area, ComposedChart
 } from 'recharts';
 
 interface ChartDataPoint {
